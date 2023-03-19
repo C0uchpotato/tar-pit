e5e5cdae8dbc9a9849de3d7847b7e7d0e739372c
Jack Helmer <rjacksonhelmer@gmail.com>
2023-03-19 08:50:30 -0400

    Refactor depricated code
    Changed `exec ` to $(())

--- tar-pit.sh
+++ tar-pit.sh
@@ -13,7 +13,7 @@ else
 	mkdir $DIR
 fi
 
-cd ~
+cd ~ || return 1
 #Look for user config file
 ls -a ~ > $DEFAULT_CONFIG
 
@@ -33,11 +33,11 @@ python3 -m pip install tarsplit > /dev/null
 
 
 #Read from either default or user config, then tar respective selections
-cd ~/.config/tar-pit
+cd ~/.config/tar-pit || return 1
 
 if [ -e $USER_CONFIG ] ; then
 
-  cd ~
+  cd ~ || return 1
   echo "User defined config found"
   echo "Making tar file, this may take a while"
   tar  -jcvhf $TAR --verbatim-files-from --files-from=$USER_CONFIG ##TODO: Progress bar
@@ -46,7 +46,7 @@ if [ -e $USER_CONFIG ] ; then
 
 else
   echo "No modified config file found, defaulting to "tar"-ing entire home directory"
-  cd ~
+  cd ~ || return 1
   echo "Making tar file, this may take a while"
   tar -jcvhf $TAR --files-from=$DEFAULT_CONFIG ##TODO: Progress bar
 fi
@@ -63,7 +63,7 @@ echo "tar file completed"
 echo "out.tar moved to ~/tar-pit"
 
 
-cd $TARGET
+cd $TARGET || return 1
 
 #Use du to dump size of Tar file
 du -b $TAR > chunk_size.txt
@@ -77,13 +77,13 @@ rm chunk_size.txt
 
 #Divide TAR_SIZE by conversion rate for bytes -> GB
 #Store final (GB) size of Tar file in $TAR_SIZE
-TAR_SIZE=`expr $TAR_SIZE / $CONVERSION`
+TAR_SIZE=$(( TAR_SIZE / CONVERSION))
 #TAR_SIZE=$(echo $(( TAR_SIZE / $CONVERSION )))
 
 #Determine how many chunks needed for tarsplit
 #Add one to ensure Tar file will always be < 7.9
-CHUNKS=`expr $TAR_SIZE / 7`
-CHUNKS=`expr $CHUNKS + 1`
+CHUNKS=$(( TAR_SIZE / 7))
+CHUNKS=$(( CHUNKS + 1))
 
 if [ $CHUNKS -lt 2 ]; then
   echo "This tar file will fit on one DVD, no reason to split"
