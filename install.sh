62edc1f5a712a94d2c8d55b5780d5fc81dabe216
Jack Helmer <rjacksonhelmer@gmail.com>
2023-03-20 08:57:55 -0400

    Changed install.sh to update/install tarsplit

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "Thank you for installing tar-pit!"
+echo "copying tar-put to /usr/local/bin"
 ##TODO: Add actual install scripts
 sudo cp tar-pit.sh /usr/local/bin
+
+echo "installing tarsplit"
+python3 -m pip install tarsplit > /dev/null
